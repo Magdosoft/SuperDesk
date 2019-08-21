887e9f2392a54e743712c011022f7cc4f6f191ba
Magdosoft <46933909+Magdosoft@users.noreply.github.com>
2019-08-21 14:20:27 +0200

    search

---
+++ Client/pageTitle.ts
@@ -0,0 +1,37 @@
+/**
+ * @ngdoc service
+ * @module superdesk.core.services
+ * @name pageTitle
+ *
+ * @description
+ * PageTitle service
+ *
+ * PageTitle service is used to save and update the title of the page.
+ * The title will be 'Superdesk {- Url} {-Desk/Workspace}
+ * Url can be: Dashboard, Monitoring, Spike, Highlights, Search, Settings
+ * If Url is search then there won't be desk
+ */
+export default angular.module('superdesk.core.services.pageTitle', [])
+    .service('pageTitle', function() {
+        this.title = 'Amac';
+        this.url = '';
+        this.workspace = '';
+
+        this.setUrl = function(url) {
+            this.url = url;
+            setTitle(this.url, this.workspace);
+        };
+
+        this.setWorkspace = function(activeWorkspace) {
+            this.workspace = activeWorkspace;
+            setTitle(this.url, this.workspace);
+        };
+
+        function setTitle(url, activeWorkspace) {
+            document.title = 'Amac' + (url ? ' - ' + url : '') + (activeWorkspace ? ' - ' + activeWorkspace : '');
+        }
+
+        this.clear = function() {
+            document.title = this.title;
+        };
+    });
