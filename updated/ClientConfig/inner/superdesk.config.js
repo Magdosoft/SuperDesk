e09ff5505efdb596b494881e82a6b95fe5bdbff3
Magdosoft <46933909+Magdosoft@users.noreply.github.com>
2019-08-26 16:57:08 +0200

    asd

---
+++ updated/ClientConfig/inner/superdesk.config.js
@@ -0,0 +1,37 @@
+/**
+ * This is the default configuration file for the Superdesk application. By default,
+ * the app will use the file with the name "superdesk.config.js" found in the current
+ * working directory, but other files may also be specified using relative paths with
+ * the SUPERDESK_CONFIG environment variable or the grunt --config flag.
+ */
+
+module.exports = function(grunt) {
+    return {
+        defaultRoute: '/search',
+        features: {
+            swimlane: {defaultNumberOfColumns: 4},
+            editor3: true,
+            qumu: true,
+            savedSearch: {
+                subscriptions: true,
+            },
+            validatePointOfInterestForImages: true,
+        },
+        auth: {google: false},
+        ingest: {
+            PROVIDER_DASHBOARD_DEFAULTS: {
+                show_log_messages: true,
+                show_ingest_count: true,
+                show_time: true,
+                log_messages: 'error',
+                show_status: true,
+            },
+            DEFAULT_SCHEDULE: {minutes: 5, seconds: 0},
+            DEFAULT_IDLE_TIME: {hours: 0, minutes: 0},
+        },
+        enabledExtensions: {
+            planning: 1,
+            // extensionFolderName: 1,
+        },
+    };
+};
